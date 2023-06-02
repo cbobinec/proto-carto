2568e97c81441a7b667f2bde58a059a6c5b39081
Cédric Bobinec <cedric.bobinec@insee.fr>
2023-06-02 17:43:23 +0200

    configuration des classes de tous

--- sandbox/js/fonctions.js
+++ sandbox/js/fonctions.js
@@ -35,24 +35,27 @@ function mettreAJourRepresentationIndicateur(map, indicateur_choisi) {
 
     stepMapbox = genererStepMapbox(configuration.classes, configuration.palette)
 
+    fillColorValue = ['step', // function
+                            expressionMapbox, // value
+                            ...stepMapbox
+                        ]
+
     // On change la représentation
-    // step : https://stackoverflow.com/a/53506912
     map.setPaintProperty('insee_carroyage200m_fill', 
                         'fill-color', 
-                        ['step', // function
-                        expressionMapbox, // value
-                        ...
-    ]);
+                        fillColorValue);
 }
 
 /*
-* Génère l'expression step mapbox permettant de réprenter 
+* Génère l'expression step mapbox permettant de répresenter les classes par couleur
+* https://stackoverflow.com/a/53506912
+* https://docs.mapbox.com/mapbox-gl-js/style-spec/expressions/#step
 */
 function genererStepMapbox(classes,palette) {
       if (classes.length > palette.length) {
         alert("La palette est trop petite pour le nombre de classes à représenter !");
       }
-      // Le premier élement est la première valeur, sans couleur associée
+      // Le premier élement est la première valeur de la palette, celle par défaut avant les seuils de classe
       resultat = [ palette_jaune_rouge[0] ]
     
       // Ensuite on alterne borne de classe, et couleur associée
